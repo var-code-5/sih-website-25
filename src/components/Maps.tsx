168d812386f7c539e5f08f05fcaea54d9a535832
var-code-5 <varshithisgod@gmail.com>
2025-09-17 15:43:33 +0530

    test: add gmaps

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -7,11 +7,14 @@ import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
 
 export default function Maps() {
     const position = { lat: 53.54992, lng: 10.00678 };
-    
+
+    const mapApiKey = process.env.NEXT_PUBLIC_MAPS_API_KEY || '';
+    const mapId = process.env.NEXT_PUBLIC_MAP_ID || '';
+
     return (
-        <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY || ''}>
+        <APIProvider apiKey={mapApiKey}>
             <div className='w-96 h-96'>
-                <Map defaultCenter={position} defaultZoom={10} mapId={process.env.NEXT_PUBLIC_MAP_ID || ''}>
+                <Map defaultCenter={position} defaultZoom={10} mapId={mapId}>
                     <AdvancedMarker position={position} />
                 </Map>
             </div>
